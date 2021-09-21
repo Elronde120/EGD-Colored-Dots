8523106167419577915eefa85eac3c1568135242
Sean O <orelus@rpi.edu>
2021-09-21 13:16:02 -0400

    Added corutine to screenshot script to disable objects when a screenshot is taken

--- Assets/Scripts/ImageSaving/CameraToImageCapture.cs
+++ Assets/Scripts/ImageSaving/CameraToImageCapture.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class CameraToImageCapture : MonoBehaviour
 {
+    [SerializeField] private GameObject[] objectsToDisable;
     public void CaptureScreen(string filePath)
     {
-        ScreenCapture.CaptureScreenshot(filePath);
+        StartCoroutine(CaptureScreenCor(filePath, objectsToDisable));
+    }
+
+    private IEnumerator CaptureScreenCor(string _filePath, GameObject[] _objectsToDisable)
+    {
+        foreach (var obj in _objectsToDisable)
+        {
+            obj.SetActive(false);
+        }
+        
+        //TODO: Account for Mobile, since it returns immediatly unlike PC
+        yield return new WaitForFixedUpdate();
+        ScreenCapture.CaptureScreenshot(_filePath);
+        yield return new WaitForFixedUpdate();
+        
+        foreach (var obj in _objectsToDisable)
+        {
+            obj.SetActive(true);
+        }
     }
 }
