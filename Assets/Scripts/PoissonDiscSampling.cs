2f390a40fd701e372253308ad82ff1b6f652ef71
DennisChen11214 <87625335+DennisChen11214@users.noreply.github.com>
2021-09-23 19:59:24 -0400

    elementary spawning works

--- Assets/Scripts/PoissonDiscSampling.cs
+++ Assets/Scripts/PoissonDiscSampling.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public static class PoissonDiscSampling{
 
     //Returns a list of all the positions of the dots and their radii
-    public static List<(Vector2, float)> GeneratePoints(float minRadius, float maxRadius, Vector2 region, int numSamplesBeforeRejection = 30){
+    public static List<(Vector2, float)> GeneratePoints(float minRadius, float maxRadius, Vector2 region, Vector2 center, List<(Vector2, float)> points, bool initialSpawn, int numSamplesBeforeRejection = 30){
         float cellSize = 0.1f;
         int[,] grid = new int[Mathf.CeilToInt(region.x/cellSize),Mathf.CeilToInt(region.y/cellSize)];
-        List<(Vector2, float)> points = new List<(Vector2,float)>();
         List<(Vector2, float)> spawnPoints = new List<(Vector2, float)>();
-        spawnPoints.Add((region/2, Random.Range(minRadius, maxRadius)));
+        List<(Vector2, float)> newPoints = new List<(Vector2, float)>();
+        for(int i = 0; i < points.Count; i++){
+            Vector2 adjustedPos = points[i].Item1 - center + region / 2;
+            if(adjustedPos.x > 0 && adjustedPos.x < region.x && adjustedPos.y > 0 && adjustedPos.y < region.y){
+                grid[(int)(adjustedPos.x / cellSize),(int)(adjustedPos.y / cellSize)] = i;
+                spawnPoints.Add((adjustedPos, points[i].Item2));
+            }
+        }
+        if(spawnPoints.Count == 0)
+            spawnPoints.Add((region/2, Random.Range(minRadius, maxRadius)));
         while(spawnPoints.Count > 0){
             int spawnIndex = Random.Range(0, spawnPoints.Count);
             Vector2 spawnPoint = spawnPoints[spawnIndex].Item1;
@@ -21,8 +29,9 @@ public static class PoissonDiscSampling{
                 float angle = Random.value * 2 * Mathf.PI;
                 Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;
                 Vector2 candidate = spawnPoint + dir * Random.Range(spawnRadius + radius, spawnRadius + 2 * radius);
-                if(isValid(candidate, region, cellSize, radius, points, grid)){
-                    points.Add((candidate, radius));
+                if(isValid(candidate, region, cellSize, radius, center, points, grid, initialSpawn)){
+                    points.Add((candidate - region / 2 + center, radius));
+                    newPoints.Add((candidate - region / 2 + center, radius));
                     spawnPoints.Add((candidate, radius));
                     grid[(int)(candidate.x / cellSize),(int)(candidate.y / cellSize)] = points.Count;
                     accepted = true;
@@ -31,11 +40,11 @@ public static class PoissonDiscSampling{
             }
             if(!accepted) spawnPoints.RemoveAt(spawnIndex);
         }
-        return points;
+        return newPoints;
     }
 
     //Checks if there's any dot within this dot's radius box
-    public static bool isValid(Vector2 candidate, Vector2 region, float cellSize, float radius, List<(Vector2,float)> points, int[,] grid){
+    public static bool isValid(Vector2 candidate, Vector2 region, float cellSize, float radius, Vector2 center, List<(Vector2,float)> points, int[,] grid, bool initialSpawn){
         if(candidate.x >= 0 && candidate.x < region.x && candidate.y >= 0 && candidate.y < region.y){
             int cellRange = 20;
             int cellX = (int)(candidate.x / cellSize);
@@ -48,14 +57,19 @@ public static class PoissonDiscSampling{
                 for(int y = startCellY; y <= endCellY; y++){
                     int pointIndex = grid[x,y] - 1;
                     if(pointIndex != -1){
-                        float distance = Vector2.Distance(candidate, points[pointIndex].Item1);
-                        //This guarantees no dot collisions but there's a lot less dots
-                        // if(distance < radius + points[pointIndex].Item2){
-                        //     return false;
-                        // }
-                        //This is for more dots but they have to move apart at the beginning
-                        if(distance < radius){
-                            return false;
+                        float distance = Vector2.Distance(candidate, points[pointIndex].Item1 + region / 2 - center);
+                        //The initial spawn spawns a lot more dots
+                        if(initialSpawn){
+                            //This is for more dots but they have to move apart at the beginning
+                            if(distance < radius){
+                                return false;
+                            }
+                        }
+                        //The manual spawn only spawns if there is space
+                        else{
+                            if(distance < radius + points[pointIndex].Item2){
+                                return false;
+                            }
                         }
                     }
                 }
