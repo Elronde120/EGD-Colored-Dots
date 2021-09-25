0e2b1758dc48c052a04bef59afbae2b3678dc086
Sean O <orelus@rpi.edu>
2021-09-25 18:01:02 -0400

    Documentation

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -24,6 +24,12 @@ public class MusicManager : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Switches the current track to the given track, plays immediately
+    /// </summary>
+    /// <param name="trackIndex">The track to play on <see cref="musicTracks"/></param>
+    /// <param name="delay">How long to delay playing</param>
+    /// <returns>True if switching and playing was successful. False otherwise</returns>
     public bool SwitchTrack(int trackIndex, ulong delay = ulong.MinValue)
     {
         if (trackIndex < 0 || trackIndex >= musicTracks.Length)
@@ -41,11 +47,21 @@ public class MusicManager : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Stops the music immediately, if playing
+    /// </summary>
     public void Stop()
     {
         _source.Stop();
     }
 
+    /// <summary>
+    /// Resumes music playing if stopped. Must have a audio clip prepared.
+    /// </summary>
+    /// <remarks>
+    /// Call <see cref="SwitchTrack"/> if unsure if audio clip is prepared.
+    /// </remarks>
+    /// <param name="delay"></param>
     public void Resume(ulong delay = ulong.MinValue)
     {
         _source.Play();
